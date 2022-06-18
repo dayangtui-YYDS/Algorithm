efe4811023cb7ccce608151b16927cf7b09500a5
yucongcong <123456@qq.com>
2022-06-18 14:28:12 +0800

    最长公共子序列

--- Algorithm/src/main/java/com/yc/LongestCommonSubsequence.java
+++ Algorithm/src/main/java/com/yc/LongestCommonSubsequence.java
@@ -1,9 +1,15 @@
-package com.yc;
+package main.java.com.yc;
 
 import org.junit.Test;
 
 import java.util.Random;
 
+/**
+ * @ClassName: LongestCommonSubsequence
+ * @Description:
+ * @Author: yucongcong
+ * @Date: 2022-06-18 14:17
+ */
 // 最长公共子序列问题
 public class LongestCommonSubsequence {
     public static int LongestCommonSubsequence(String str1, String str2){
@@ -29,7 +35,8 @@ public class LongestCommonSubsequence {
         {
             for (int j=1; j< s2.length; ++j)
             {
-                // s1[i-1] != s2[j-1]
+                // s1[i-1] != s2[j-1]证明s1[i] s2[j]至少有一个不在最长公共子序列中
+                // dp[i-1][j] dp[i][j-1] dp[i-1][j-1] 而dp[i-1][j-1]的结果明显小于等于dp[i-1][j] 和dp[i][j-1] 可以直接忽略
                 dp[i][j] = s1[i-1] == s2[j-1] ? dp[i-1][j-1] + 1 : Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
@@ -37,25 +44,36 @@ public class LongestCommonSubsequence {
     }
 }
 
-class Demo
+class LongestCommonSubsequenceTestCase
 {
-    // 随机生成一个字符串
-    private String RandomString()
+    // 生成样本
+    private String CreateSample()
     {
+        // 英文字母数量
+        int letterCount = 26;
+        int numberCount = 10;
+        char[] sample = new char[letterCount<<1 + numberCount];
+        // 生成小写字母
         char low = 'a';
-        char upper = 'A';
-        int number = 0;
-        int count = random.nextInt(Bound);
-        char[] ss = new char[count]
-        for (int i=0; i<count; ++i)
-        {
-
+        for (int i=0; i< letterCount; ++i){
+            sample[i] = (char) (low + i);
         }
+        // 生成大写字母
+        char upper = 'A';
+        for (int i=0; i< letterCount; ++i){
+            sample[i] = (char) (upper + i);
+        }
+        //生成数字
+        int number = 0;
+        for (int i=0; i< letterCount; ++i){
+            sample[i] = (char) (number + i);
+        }
+        return sample.toString();
     }
 
     @Test
     public void Test(){
-
+        String sample = CreateSample();
     }
 
     private final int Bound = 20;
